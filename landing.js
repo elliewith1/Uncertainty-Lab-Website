da14d1b7476ff4dabbb2d1a0abb9671e093ea1f4
elliewith1 <131091227+elliewith1@users.noreply.github.com>
2023-04-26 20:23:08 -0700

    Update landing.js

--- landing.js
+++ landing.js
@@ -3,5 +3,5 @@ const commit_btn = document.getElementById("commit-btn");
 commit_btn.addEventListener("click", handle_commit);
 
 function handle_commit() {
-  window.location.href = "signup.html";
+  window.location.href = "signuppage.php";
 }
